1dc930a58eabf703c196b0982625b00a493f07ca
JalzAe <jalzae@gmail.com>
2023-09-07 13:42:55 +0700

    add mapping barang

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ workbook.xlsx.readFile(excelFilePath)
     sheetData.forEach((sheetJSON, index) => {
       if (index > 0) {
         // console.log(`Saved ${sheetJSON.sheetName}`);
-        const satuan = []
+
         if (sheetJSON.sheetName == 'Satuan') {
           const dataSatuan = sheetJSON.data;
           const headerSatuan = dataSatuan[0]
@@ -69,10 +69,48 @@ workbook.xlsx.readFile(excelFilePath)
 
         }
         if (sheetJSON.sheetName == 'Varian & SKU') {
+          //'*Kategori Varian', 'Urutan Varian', '*Varian', 'Kode Varian'
+          const dataVarian = sheetJSON.data;
+          const headerVarian = dataVarian[0]
+          const bodyVarian = dataVarian.slice(1)
+          const indexKategoriVarian = headerVarian.findIndex(e => e === '*Kategori Varian')
+          const indexUrutanVarian = headerVarian.findIndex(e => e === 'Urutan Varian')
+          const indexVarian = headerVarian.findIndex(e => e === '*Varian')
+          const indexKodeVarian = headerVarian.findIndex(e => e === 'Kode Varian')
+          bodyVarian.forEach((e) => {
+            json.varian.push({
+              kategori_varian: e[indexKategoriVarian],
+              urutan: e[indexUrutanVarian],
+              nama_varian: e[indexVarian].split(','),
+              kode_varian: e[indexKodeVarian].split(','),
+            })
+          })
 
         }
         if (sheetJSON.sheetName == 'Data Barang') {
-
+          //*Mode Pengadaan	*Satuan Terkecil	Satuan Lain	*Barang dijual	*Tipe Barang	*Memiliki Varian	Varian SKU	Harga Jual Satuan	Harga Pokok/Standart	Harga Jual Minimum	Harga Beli Maksimum	Barcode	Kode SKU
+          const dataBarang = sheetJSON.data;
+          const headerBarang = dataBarang[0]
+          const bodyBarang = dataBarang.slice(1)
+          const indexNamaBarang = headerBarang.findIndex(e => e === '*Nama Barang')
+          const indexJenisBarang = headerBarang.findIndex(e => e === '*Jenis Barang')
+          const indexModePengadaan = headerBarang.findIndex(e => e === '*Mode Pengadaan')
+          const indexSatuanTerkecil = headerBarang.findIndex(e => e === '*Satuan Terkecil')
+          const indexSatuanLain = headerBarang.findIndex(e => e === 'Satuan Lain')
+          const indexBarangDijual = headerBarang.findIndex(e => e === '*Barang dijual')
+          const indexTipeBarang = headerBarang.findIndex(e => e === '*Tipe Barang')
+          const indexMemilikiVarian = headerBarang.findIndex(e => e === '*Memiliki Varian')
+          const indexVarianSKU = headerBarang.findIndex(e => e === 'Varian SKU')
+          const indexHargaJualSatuan = headerBarang.findIndex(e => e === 'Harga Jual Satuan')
+          const indexHargaPokok = headerBarang.findIndex(e => e === 'Harga Pokok/Standart')
+          const indexHargaJualMinimum = headerBarang.findIndex(e => e === 'Harga Jual Minimum')
+          const indexBarcode = headerBarang.findIndex(e => e === 'Barcode')
+          const indexKodeSKU = headerBarang.findIndex(e => e === 'Kode SKU')
+          bodyBarang.forEach((e) => {
+            json.barang.push({
+              nama_barang: e[indexNamaBarang],
+            })
+          })
         }
       }
     });
