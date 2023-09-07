eaef61616507e898b5349f486b199068dd4d4178
JalzAe <jalzae@gmail.com>
2023-09-07 11:27:45 +0700

    update read barang

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const workbook = new ExcelJS.Workbook();
 const excelFilePath = './upload.xlsx'; // Replace with the path to your Excel file
 
+let json = []
 workbook.xlsx.readFile(excelFilePath)
   .then(() => {
     const sheetData = [];
@@ -29,15 +30,32 @@ workbook.xlsx.readFile(excelFilePath)
       };
 
       sheetData.push(sheetJSON);
-    });
 
+    });
+    const json = { satuan: [], barang: [], varian: [] }
     sheetData.forEach((sheetJSON, index) => {
       if (index > 0) {
-        console.log(`Saved ${sheetJSON.sheetName}`);
-        console.log(`Data`, sheetJSON.data)
+        // console.log(`Saved ${sheetJSON.sheetName}`);
+        const satuan = []
+        if (sheetJSON.sheetName == 'Satuan') {
+          const dataSatuan = sheetJSON.data;
+          const headerSatuan = dataSatuan[0]
+          const bodySatuan = dataSatuan.slice(1)
+          const indexNamaSatuan = headerSatuan.findIndex(e => e === '*Nama Satuan')
+
+          bodySatuan.forEach((e) => {
+            json.satuan.push({
+              nama_satuan: e[indexNamaSatuan]
+            })
+          })
+        }
       }
     });
   })
   .catch((error) => {
     console.error('Error reading Excel file:', error);
   });
+
+
+
+
